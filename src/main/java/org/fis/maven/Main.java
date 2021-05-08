6617d9c4fd33bf27a4d930ade55431b92cc7ac6f
FloareEliza <eliza.floare@gmail.com>
2021-05-08 14:24:44 +0300

    Updated Pom and Main

--- src/main/java/org/fis/maven/Main.java
+++ src/main/java/org/fis/maven/Main.java
@@ -1,17 +1,11 @@
 package org.fis.maven;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class Main {
-    public static void main(String[] args)
-    {
-        Logger LOGGER= LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
 
-        LOGGER.trace("This is a TRACE log");
-        LOGGER.debug("This is a DEBUG log");
-        LOGGER.info("This is a INFO log");
-        LOGGER.warn("This is a WARN log");
-        LOGGER.error("This is an ERROR log");
+        System.out.println(".cbecmiesfsefewio");
+
     }
 }
