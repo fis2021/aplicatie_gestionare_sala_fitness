e729abaf90d132f16883a2a21a32c5f75eafb08f
Calarasu Oana <oanacalarasu2@gmail.com>
2021-05-12 22:13:03 +0300

    Updated AdministratorPageController file

--- src/main/java/org/fis/maven/controllers/AdminPageController.java
+++ src/main/java/org/fis/maven/controllers/AdminPageController.java
@@ -21,14 +21,22 @@ public class AdminPageController {
     @FXML
     private Button edittrainersButton;
 
-
-
     @FXML
     public void handleLogout(javafx.event.ActionEvent login) throws IOException {
         FXMLLoader Loader = new FXMLLoader();
         Loader.setLocation(getClass().getClassLoader().getResource("login.fxml"));
         Parent viewLogin = Loader.load();
         Scene LoginScene = new Scene(viewLogin, 650, 450);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
+    }
+    @FXML
+    public void editProgram(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("editProgramAdmin.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 880, 680);
         Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
         window.setScene(LoginScene);
         window.show();
