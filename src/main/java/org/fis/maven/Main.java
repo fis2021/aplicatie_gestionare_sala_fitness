dbb4508d0c23e11644afad1fecff1c1ac3b04736
Calarasu Oana <oanacalarasu2@gmail.com>
2021-05-12 22:24:42 +0300

    Updated Main

--- src/main/java/org/fis/maven/Main.java
+++ src/main/java/org/fis/maven/Main.java
@@ -6,8 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.fis.maven.services.FileSystemService;
+import org.fis.maven.services.GymProgramService;
 import org.fis.maven.services.UserService;
-import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 public class Main extends Application {
@@ -15,6 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         initDirectory();
         UserService.initDatabase();
+        GymProgramService.initDatabase();
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Aplicatie Gestionare Sala de Fitness");
         primaryStage.setScene(new Scene(root, 650, 450));
