221f77f1ca0794ec377a935e3d136ea14e9cb6a6
FloareEliza <eliza.floare@gmail.com>
2021-05-13 15:46:34 +0300

    Updated User and UserService

---
+++ src/main/java/org/fis/maven/model/User.java
@@ -0,0 +1,91 @@
+package org.fis.maven.model;
+
+import org.dizitart.no2.objects.Id;
+
+import java.util.Objects;
+
+public class User {
+    @Id
+    private String username;
+    private String password;
+    private String role;
+    private String eMail, phoneNumber, name, personalKey;
+
+    public User(String username, String password, String role, String name, String eMail, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+    }
+
+    public User(String username, String password, String role, String name, String eMail, String phoneNumber, String personalKey) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.name = name;
+        this.personalKey = personalKey;
+    }
+
+    public User() {
+
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public String getphoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(username, user.username)) return false;
+        if (!Objects.equals(password, user.password)) return false;
+        return true;
+    }
+
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+}
