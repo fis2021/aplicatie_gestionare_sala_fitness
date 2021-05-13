514bbd55130d0356bd63a1358901bff0cb9aa15b
FloareEliza <eliza.floare@gmail.com>
2021-05-13 18:58:48 +0300

    Updated trainerPageController

---
+++ src/main/java/org/fis/maven/controllers/TrainerPageController.java
@@ -0,0 +1,43 @@
+package org.fis.maven.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TrainerPageController {
+
+    @FXML
+    private Button addClientDetailsButton;
+    @FXML
+    private Button viewClientsButton;
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    public void handleLogout(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("login.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 650, 450);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
+    }
+
+    @FXML
+    public void handleaddClientDetails(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("editPersonalEvolutionTrainer.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 832, 665);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
+    }
+}
