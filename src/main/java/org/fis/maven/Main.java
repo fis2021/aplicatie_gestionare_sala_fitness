fc7416a889200bb64e8e5fe86b60addef5669ff2
FloareEliza <eliza.floare@gmail.com>
2021-05-15 22:34:53 +0300

    Updated main

--- src/main/java/org/fis/maven/Main.java
+++ src/main/java/org/fis/maven/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        initDirectory();
+
         UserService.initDatabase();
         GymProgramService.initDatabase();
         CustomerEvolutionService.initDatabase();
@@ -30,11 +30,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    private void initDirectory() {
-        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
-        if (!Files.exists(applicationHomePath))
-            applicationHomePath.toFile().mkdirs();
-    }
+
 
 
     public static void main(String[] args) {
